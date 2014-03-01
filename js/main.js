de67e3012fdcdd13d92e8b420c3fa02b00ef45dd
Braden Groom <braden.groom@gmail.com>
2014-03-01 13:44:19 -0600

    added listener for spacebar

--- js/main.js
+++ js/main.js
@@ -9,12 +9,22 @@ Game = {
 	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
     	.color('rgb(255,0,0)')
     	.attr({ x: window.innerWidth/2, y: 20, w: 75, h: 15 })
-    	.multiway(4, { A: 180, D: 0 });
+    	.multiway(10, { A: 180, D: 0 });
 
 	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
     	.color('rgb(0,0,255)')
     	.attr({ x: window.innerWidth/2, y: window.innerHeight-50, w: 75, h: 15 })
-    	.multiway(4, { LEFT_ARROW: 180, RIGHT_ARROW: 0 });
+    	.multiway(10, { LEFT_ARROW: 180, RIGHT_ARROW: 0 })
+    	.bind("KeyDown",function(frame){
+    		if(frame.keyCode === Crafty.keys.SPACE){
+                this.shoot();
+            } 
+            
+        });
+
+    
 
 	}
+
+
 }
