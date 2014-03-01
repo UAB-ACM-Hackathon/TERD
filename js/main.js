89fffc0e107295e10a0ac08c0eb3fa585d135eb8
Erby Fischer <erbycf@uab.edu>
2014-03-01 14:42:33 -0600

    YESSSS

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
 Game = {
 	start: function () {
-		Crafty.init(window.innerWidth, 
-			window.innerHeight);
+		Crafty.init(window.innerWidth, window.innerHeight);
 
-	Crafty.background('black');
+		Crafty.background('black');
 
-	//Aircraft
-	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
-    	.color('rgb(255,0,0)')
-    	.attr({ x: window.innerWidth/2, y: 20, w: 75, h: 15 })
-    	.multiway(10, { A: 180, D: 0 });
-
-	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
-    	.color('rgb(0,0,255)')
-    	.attr({ x: window.innerWidth/2, y: window.innerHeight-50, w: 75, h: 15 })
-    	.multiway(10, { LEFT_ARROW: 180, RIGHT_ARROW: 0 })
-    	.bind("KeyDown",function(frame){
-    		if(frame.keyCode === Crafty.keys.SPACE){
-                this.shoot();
-            } 
-            
-        });
-
-    
-
+		var player_top = Crafty.e("Player").player(LOCATION.TOP);
+		var player_bottom = Crafty.e("Player").player(LOCATION.BOTTOM);
 	}
-
-
 }
---
+++ js/player.js
@@ -0,0 +1,40 @@
+var LOCATION = {
+	TOP: 0,
+	BOTTOM: 1
+};
+
+function createMultiway(location) {
+	if (location === LOCATION.TOP) {
+		return { A: 180, D: 0 };
+	} else {
+		return { LEFT_ARROW: 180, RIGHT_ARROW: 0 };
+	}
+}
+
+function createAttr(location) {
+	if (location === LOCATION.TOP) {
+		return { x: window.innerWidth / 2, y: 20, w: 75, h: 15 };
+	} else {
+		return { x: window.innerWidth / 2, y: window.innerHeight - 50, w: 75, h: 15 };
+	}
+}
+
+Crafty.c("Player", {
+	movementSpeed: 8,
+	lives: 1,
+	score: 0,
+	fireRate: 4,
+	location: LOCATION.BOTTOM,
+	init: function() {
+		
+	},
+
+	player: function(location) {
+		this.location = location;
+		this.requires("2D, DOM, Color, Multiway, Keyboard, Collision")
+		.multiway(this.movementSpeed, 
+			createMultiway(this.location))
+		.attr(createAttr(this.location))
+		.color("red");
+	}
+});
