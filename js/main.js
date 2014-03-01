b7487103b77d1ea7eb204b7c1deb7f3f05f6363e
Braden Groom <braden.groom@gmail.com>
2014-03-01 13:19:32 -0600

    added controlable boxes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 Game = {
 	start: function () {
-		Crafty.init(480, 320);
-		Crafty.background('green');
+		Crafty.init(window.innerWidth, 
+			window.innerHeight);
+
+	Crafty.background('black');
+
+	//Paddles
+	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+    	.color('rgb(255,0,0)')
+    	.attr({ x: window.innerWidth/2, y: 20, w: 75, h: 15 })
+    	.multiway(4, { A: 180, D: 0 });
+
+	Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+    	.color('rgb(0,0,255)')
+    	.attr({ x: window.innerWidth/2, y: window.innerHeight-20, w: 75, h: 15 })
+    	.multiway(4, { LEFT_ARROW: 180, RIGHT_ARROW: 0 });
+
 	}
 }
