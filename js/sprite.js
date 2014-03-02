e556dc7639ad9ba245eb4f2d2627643512b47fc3
Braden Groom <braden.groom@gmail.com>
2014-03-01 19:51:54 -0600

    sprites

---
+++ js/sprite.js
@@ -0,0 +1,15 @@
+Crafty.sprite("images/ship_bottom.png",{
+	ship_bottom:[0,0,85,85]
+});
+
+Crafty.sprite("images/ship_top.png",{
+	ship_top:[0,0,85,85]
+});
+
+Crafty.sprite("images/enemy_top.png",{
+    badship_top:[0,0,100,88]
+});
+
+Crafty.sprite("images/enemy_bottom.png",{
+    badship_bottom:[0,0,100,88]
+});
